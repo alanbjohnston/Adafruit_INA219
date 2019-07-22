28344a733518f271035729f3930d0b1d148355d0
alanbjohnston <alan.b.johnston@gmail.com>
2019-07-22 13:21:26 -0400

    added global uint16_t ina219_config

--- Adafruit_INA219.h
+++ Adafruit_INA219.h
@@ -139,6 +139,7 @@ enum {
   uint8_t ina219_i2caddr;
   //uint32_t ina219_calValue;
   uint16_t ina219_calValue;
+  uint16_t ina219_config;
   // The following multipliers are used to convert raw current and power
   // values to mA and mW, taking into account the current config settings
   uint32_t ina219_currentDivider_mA;
